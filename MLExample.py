fcc52950e77eb0adef907da9d72cc6a26b8f1f92
Ryan <ryanxipingzhong@gmail.com>
2025-09-06 20:02:50 -0400

    Added training data, and predictions

--- MLExample.py
+++ MLExample.py
@@ -1,20 +1,43 @@
-# Code you have previously used to load data
-import pandas as pd
-from sklearn.tree import DecisionTreeRegressor
+import pandas as pd #used for reading and data manipulation
+from sklearn.metrics import mean_absolute_error #used to measure model performance
+from sklearn.model_selection import train_test_split #used to split data into training and validation sets
+from sklearn.tree import DecisionTreeRegressor #used to create the model
+
 
 # Path of the file to read
-iowa_file_path = 'MLExample.csv'
+example_file_path = 'MLExample.csv'
 
-home_data = pd.read_csv(iowa_file_path)
-y = home_data.Salary
-feature_columns = ['Age']
-X = home_data[feature_columns]
+#This reads the data into a pandas DataFrame
+example_data = pd.read_csv(example_file_path)
 
-# Specify Model
-iowa_model = DecisionTreeRegressor()
-# Fit Model
-iowa_model.fit(X, y)
+# Create target object and call it y
+y = example_data.Salary
+# Create X(The feature(s) used to make the prediction)
+features = ['Age']
+X = example_data[features]
 
-print("First in-sample predictions:", iowa_model.predict(X.head()))
-print("Test")
-print("Actual target values for those homes:", y.head().tolist())
+# Split into validation and training data(train_X and train_y are the training data, while val_X and val_y are the validation data)
+#random_state is set to 1 to ensure reproducibility
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+# Creates the model.
+# Setting random_state to 1 to ensure same results each run
+iowa_model = DecisionTreeRegressor(random_state=1)
+# Fits the model into the training data
+iowa_model.fit(train_X, train_y)
+
+#predict() function predicts the salaries for the validation data
+val_predictions = iowa_model.predict(val_X)
+#mean_absolute_error() function computes the mean absolute error between the predicted and actual salaries
+#If mae is low, the model is performing well
+val_mae = mean_absolute_error(val_predictions, val_y)
+print("Validation MAE when not specifying max_leaf_nodes: {:,.0f}".format(val_mae))
+
+# Model is trained again with a specific max_leaf_nodes value(leaf_nodes are the end points of the tree where predictions are made)
+#Limiting them makes the model simpler and can help prevent overfitting
+iowa_model = DecisionTreeRegressor(max_leaf_nodes=100, random_state=1)
+iowa_model.fit(train_X, train_y)
+val_predictions = iowa_model.predict(val_X)
+#evaluated with MAE again
+val_mae = mean_absolute_error(val_predictions, val_y)
+print("Validation MAE for best value of max_leaf_nodes: {:,.0f}".format(val_mae))
